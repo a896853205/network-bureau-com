3a2c9c50385adefb6ef497cc53d1a575636fefc0
Qccccc's laptop <896853205@qq.com>
2019-12-09 13:45:04 +0800

    完成登录页基本骨架

--- src/page/Login-controller.jsx
+++ src/page/Login-controller.jsx
@@ -1,5 +1,33 @@
 import React from 'react';
+import '@/style/login.styl';
 
 export default props => {
-  return <div>登录</div>;
+  return (
+    <div className='login-box'>
+      <div className='login-inner-box'>
+        <div className='left-box'>
+          <img src='' alt='' />
+          <div>
+            <div>
+              <h5>登录</h5>
+              <span></span>
+            </div>
+            <input type='text' />
+            <input type='text' />
+            {/* <a href=''></a> */}
+            <button></button>
+          </div>
+          <ol>
+            <li></li>
+            <li></li>
+            <li></li>
+          </ol>
+        </div>
+        <div className='right-box'>
+          <h1>业务管理系统</h1>
+          <p>欢迎使用业务管理系统,使您快速方便地办理各类软件测试。</p>
+        </div>
+      </div>
+    </div>
+  );
 };
