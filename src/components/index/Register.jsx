4d9ee7b1504acdfa6afd75c7a784b57634b0fddb
cx <chenxiao120660@hotmail.com>
2019-12-11 11:41:33 +0800

    增加注册

--- src/components/index/Register.jsx
+++ src/components/index/Register.jsx
@@ -1,51 +1,126 @@
 import React from 'react';
 
 // 样式
-import { Icon, Input, Form, Button } from 'antd';
+import { Icon, Input, Form, Button, message } from 'antd';
 import '@/style/register.styl';
 
+import proxyFetch from '@/util/request';
+import { CREATE_NEW_ENTERPRISE } from '@/constants/api-constants';
+import schema from 'async-validator';
 // 路由
 import { Link } from 'react-router-dom';
 
-export default props => {
+const WrappedNormalLoginForm = Form.create({ name: 'normal_login' })(props => {
+  const { getFieldDecorator } = props.form;
+  /**
+   *
+   *  注册按钮
+   */
+  const handleSubmit = e => {
+    e.preventDefault();
+    props.form.validateFields(async (err, values) => {
+      if (values.password !== values.passwordAgain) {
+        info();
+      } else {
+        if (!err) {
+          delete values.passwordAgain;
+          let data = await proxyFetch(CREATE_NEW_ENTERPRISE, values);
+        }
+      }
+    });
+  };
+
+  const info = () => {
+    message.info('两次密码必须相同');
+  };
+
   return (
-    <Form>
+    <Form onSubmit={handleSubmit}>
       <Form.Item>
-        <Input
-          prefix={<Icon type='user' style={{ color: 'rgba(0,0,0,.25)' }} />}
-          placeholder='统一社会信用代码'
-          size='large'
-        />
+        {getFieldDecorator('code', {
+          rules: [
+            {
+              required: true,
+              message: '请输入统一社会信用代码！'
+            },
+            {
+              pattern: /^[0-9]{6}[0-3][1-2][0-9]{6}[0-9]$/,
+              message: '统一社会信用代码不符合规则'
+            }
+          ]
+        })(
+          <Input
+            prefix={<Icon type="user" style={{ color: 'rgba(0,0,0,.25)' }} />}
+            placeholder="统一社会信用代码"
+            size="large"
+          />
+        )}
       </Form.Item>
       <Form.Item>
-        <Input
-          prefix={<Icon type='user' style={{ color: 'rgba(0,0,0,.25)' }} />}
-          placeholder='企业名称'
-          size='large'
-        />
+        {getFieldDecorator('name', {
+          rules: [{ required: true, message: '请输入企业名称！' }]
+        })(
+          <Input
+            prefix={<Icon type="user" style={{ color: 'rgba(0,0,0,.25)' }} />}
+            placeholder="企业名称"
+            size="large"
+          />
+        )}
       </Form.Item>
       <Form.Item>
-        <Input
-          prefix={<Icon type='key' style={{ color: 'rgba(0,0,0,.25)' }} />}
-          placeholder='密码'
-          size='large'
-          type='password'
-        />
+        {getFieldDecorator('phone', {
+          rules: [
+            {
+              required: true,
+              message: '请输入电话！'
+            },
+            {
+              pattern: /^[1][0-9][0-9]{9}$/,
+              message: '电话号码不符合规则'
+            }
+          ]
+        })(
+          <Input
+            prefix={<Icon type="user" style={{ color: 'rgba(0,0,0,.25)' }} />}
+            placeholder="电话"
+            size="large"
+          />
+        )}
       </Form.Item>
       <Form.Item>
-        <Input
-          prefix={<Icon type='key' style={{ color: 'rgba(0,0,0,.25)' }} />}
-          placeholder='确认密码'
-          size='large'
-          type='password'
-        />
+        {getFieldDecorator('password', {
+          rules: [{ required: true, message: '请输入密码！' }]
+        })(
+          <Input
+            prefix={<Icon type="key" style={{ color: 'rgba(0,0,0,.25)' }} />}
+            placeholder="密码"
+            size="large"
+            type="password"
+          />
+        )}
       </Form.Item>
       <Form.Item>
-        <div className='register-button-box'>
-          <Button type='primary'>注册</Button>
-          <Link to='/index/login'>我有账号</Link>
+        {getFieldDecorator('passwordAgain', {
+          rules: [{ required: true, message: '请确认密码！' }]
+        })(
+          <Input
+            prefix={<Icon type="key" style={{ color: 'rgba(0,0,0,.25)' }} />}
+            placeholder="确认密码"
+            size="large"
+            type="password"
+          />
+        )}
+      </Form.Item>
+      <Form.Item>
+        <div className="register-button-box">
+          <Button type="primary" htmlType="submit">
+            注册
+          </Button>
+          <Link to="/index/login">我有账号</Link>
         </div>
       </Form.Item>
     </Form>
   );
-};
+});
+
+export default WrappedNormalLoginForm;
--- src/page/Index-controller.jsx
+++ src/page/Index-controller.jsx
@@ -16,54 +16,54 @@ export default ({ params }) => {
   const isRegister = type === 'register';
 
   return (
-    <div className='index-box'>
-      <div className='index-inner-box'>
-        <div className='left-box'>
-          <h1 className='logo'>
-            <Icon type='reconciliation' /> <span>业务管理系统</span>
+    <div className="index-box">
+      <div className="index-inner-box">
+        <div className="left-box">
+          <h1 className="logo">
+            <Icon type="reconciliation" /> <span>业务管理系统</span>
           </h1>
-          <div className='nav'>
+          <div className="nav">
             <Link
-              to='/index/login'
+              to="/index/login"
               className={`${!isRegister ? 'focus' : 'unfocus'} button`}
             >
               登录
             </Link>
             <Link
-              to='/index/register'
+              to="/index/register"
               className={`${isRegister ? 'focus' : 'unfocus'} button`}
             >
               注册
             </Link>
           </div>
-          <div className='form'>
+          <div className="form">
             {isRegister ? <RegisterComponent /> : <LoginComponent />}
           </div>
           {isRegister ? (
             undefined
           ) : (
-            <ul className='performance'>
+            <ul className="performance">
               <li>
-                <Icon type='clock-circle' className='icon' />
+                <Icon type="clock-circle" className="icon" />
                 <span>更快捷的业务办理</span>
               </li>
               <li>
-                <Icon type='bug' className='icon' />
+                <Icon type="bug" className="icon" />
                 <span>更稳定的测试流程</span>
               </li>
               <li>
-                <Icon type='safety-certificate' className='icon' />
+                <Icon type="safety-certificate" className="icon" />
                 <span>更安全的项目管理</span>
               </li>
             </ul>
           )}
         </div>
-        <div className='right-box'>
+        <div className="right-box">
           <div>
-            <p className='main-describe'>欢迎使用</p>
-            <p className='main-describe'>软件测试</p>
-            <p className='main-describe'>业务管理系统</p>
-            <p className='sub-describe'>
+            <p className="main-describe">欢迎使用</p>
+            <p className="main-describe">软件测试</p>
+            <p className="main-describe">业务管理系统</p>
+            <p className="sub-describe">
               欢迎使用软件测试业务管理系统,使您快速方便地办理各类软件测试。
             </p>
           </div>
