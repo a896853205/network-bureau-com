8abbf570b55c2f78c02929cbed86d929d04b2b19
Qccccc's laptop <896853205@qq.com>
2019-12-12 11:09:06 +0800

    添加了三个登记测试的路由

--- src/page/home/Home-controller.jsx
+++ src/page/home/Home-controller.jsx
@@ -1,11 +1,14 @@
 import React from 'react';
 
 // 路由
-import { useRouteMatch } from 'react-router-dom';
+import { useRouteMatch, Link } from 'react-router-dom';
 import * as ROUTES from '@/constants/route-constants';
 
 // controller
 import HomeIndexController from '@/page/home/Home-index-controller.jsx';
+import RegistrationWelcomeController from '@/page/home/registration/Registration-welcome-controller.jsx';
+import RegistrationProcessController from '@/page/home/registration/Registration-process-controller.jsx';
+import RegistrationListController from '@/page/home/registration/Registration-list-controller.jsx';
 
 // 样式
 import '@/style/home/home.styl';
@@ -15,15 +18,33 @@ const { Header, Content, Footer, Sider } = Layout,
 
 export default props => {
   const homeIndex = useRouteMatch({
-    path: ROUTES.HOME_INDEX.path,
-    exact: true
-  });
+      path: ROUTES.HOME_INDEX.path,
+      exact: true
+    }),
+    registrationWelcome = useRouteMatch({
+      path: ROUTES.HOME_REGISTRATION_WELCOME.path,
+      exact: true
+    }),
+    registrationProcess = useRouteMatch({
+      path: ROUTES.HOME_REGISTRATION_PROCESS.path,
+      exact: true
+    }),
+    registrationList = useRouteMatch({
+      path: ROUTES.HOME_REGISTRATION_LIST.path,
+      exact: true
+    });
 
   let content = null;
 
   if (homeIndex) {
     // 主首页
     content = <HomeIndexController />;
+  } else if (registrationWelcome) {
+    content = <RegistrationWelcomeController />;
+  } else if (registrationProcess) {
+    content = <RegistrationProcessController />;
+  } else if (registrationList) {
+    content = <RegistrationListController />;
   }
 
   return (
@@ -43,8 +64,14 @@ export default props => {
               </span>
             }
           >
-            <Menu.Item key='registerDeal'>办理测试</Menu.Item>
-            <Menu.Item key='registerList'>查看测试</Menu.Item>
+            <Menu.Item key='registrationDeal'>
+              <Link to={ROUTES.HOME_REGISTRATION_WELCOME.path}>办理新测试</Link>
+            </Menu.Item>
+            <Menu.Item key='registrationList'>
+              <Link to={ROUTES.HOME_REGISTRATION_LIST.path}>
+                查看进行的测试
+              </Link>
+            </Menu.Item>
           </SubMenu>
           <SubMenu
             key='entrust'
@@ -55,8 +82,8 @@ export default props => {
               </span>
             }
           >
-            <Menu.Item key='entrustDeal'>办理测试</Menu.Item>
-            <Menu.Item key='entrustList'>查看测试</Menu.Item>
+            <Menu.Item key='entrustDeal'>办理新测试</Menu.Item>
+            <Menu.Item key='entrustList'>查看进行的测试</Menu.Item>
           </SubMenu>
           <SubMenu
             key='entrustContract'
@@ -67,8 +94,8 @@ export default props => {
               </span>
             }
           >
-            <Menu.Item key='entrustContractDeal'>办理委托合同</Menu.Item>
-            <Menu.Item key='entrustContractList'>查看委托合同</Menu.Item>
+            <Menu.Item key='entrustContractDeal'>办理新委托合同</Menu.Item>
+            <Menu.Item key='entrustContractList'>查看进行的委托合同</Menu.Item>
           </SubMenu>
           <Menu.Item key='4'>
             <Icon type='setting' />
---
+++ src/page/home/registration/Registration-process-controller.jsx
@@ -0,0 +1,5 @@
+import React from 'react';
+
+export default props => {
+  return <div>登记测试流程页</div>;
+};
