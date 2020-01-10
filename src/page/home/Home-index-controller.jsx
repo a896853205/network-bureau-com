a7c4355403d3e9c62355729a99ae244cf954fc27
Qccccc's laptop <896853205@qq.com>
2020-01-10 20:24:44 +0800

    将process的样式和html结构重构了一遍

--- src/page/home/Home-index-controller.jsx
+++ src/page/home/Home-index-controller.jsx
@@ -17,10 +17,10 @@ export default props => {
           <Icon type='deployment-unit' className='icon' />
           <div className='inner-box'>
             <h2 className='caption'>软件登记测试</h2>
-            <p className='sub-passage'>
+            <p className='sucurrent-profile-inner-left-box'>
               软件产品登记测试是指检测机构按照委托方提供的测试功能点，对其指定的软件产品进行功能性的检测和验证，确保这些功能都得以实现并能正常运行。
             </p>
-            <p className='sub-passage'>
+            <p className='sucurrent-profile-inner-left-box'>
               同时，软件产品登记测试的报告也是申请软件产品登记所必须的条件，对于审查方来说第三方检测机构出具的测试报告是具有较高的参考价值。进行软件产品登记所需的测试报告,软件检测机构出具的检测证明材料，须由双软认定网上指定的检测中心出具，当地的软件协会也可以出具报告。
             </p>
           </div>
@@ -36,11 +36,11 @@ export default props => {
           <Icon type='gateway' className='icon' />
           <div className='inner-box'>
             <h2 className='caption'>软件委托测试</h2>
-            <p className='sub-passage'>
+            <p className='sucurrent-profile-inner-left-box'>
               委托测试过程中,由甲方提供符合交付要求的受测软件产品及相关文档，包括产品功能列表，需求分析，设计文档，用户文档至乙方,并指派专人配合乙方测试工作，
               并提供必要的技术培训和技术协助,并按照合同约定支付所有费用。
             </p>
-            <p className='sub-passage'>
+            <p className='sucurrent-profile-inner-left-box'>
               乙方按照国家软件质量测试标准和测试规范，完成甲方委托的软件产品的产品登记测试，在测试过程中，定期知会甲方受测软件在测试过程中出现的问题,最后出具相应的评测报告。
             </p>
           </div>
@@ -56,7 +56,7 @@ export default props => {
           <Icon type='radar-chart' className='icon' />
           <div className='inner-box'>
             <h2 className='caption'>软件测试委托合同</h2>
-            <p className='sub-passage'>
+            <p className='sucurrent-profile-inner-left-box'>
               软件测试委托合同的内容应包括甲乙双方、联系人、联系方式、地址等基本信息，详细描述双方的主要任务，确定履约地点和合同价款、测试费用支付方式、履约的期限、资料的保密性、风险责任承担、争议解决等，最终由两方签章方可生效，一式两份，甲乙双方应协商达成保密协议，未经对方书面允许，任何一方不得向第三方透露对方的任何商业机密。
             </p>
           </div>
--- src/page/home/registration/Registration-process-controller.jsx
+++ src/page/home/registration/Registration-process-controller.jsx
@@ -2,138 +2,141 @@ import React from 'react';
 import '@/style/home/registration/process.styl';
 
 // 样式
-import { Timeline, Upload, Button, Icon } from 'antd';
+import { Timeline, Icon, Rate } from 'antd';
 
 export default props => {
-
-
   return (
     <div className='registion-process-box'>
-      <div className='a-box'>
-        <p className='a-top-box'>
-          测试进度
-        </p>
-        <div className='a-bottom-box'>
-          <Timeline >
-            <Timeline.Item color="green" className='a-step'>
+      <div className='process-item-box'>
+        <p className='title-box'>测试进度</p>
+        <div className='process-profile-bottom-box'>
+          <Timeline>
+            <Timeline.Item color='green' className='process-profile-step'>
               提交上传7种材料
-          </Timeline.Item>
-            <Timeline.Item color="green" className='a-step'>
+            </Timeline.Item>
+            <Timeline.Item color='green' className='process-profile-step'>
               电子签合同
-          </Timeline.Item>
-            <Timeline.Item color="green" className='a-step'>
+            </Timeline.Item>
+            <Timeline.Item color='green' className='process-profile-step'>
               支付汇款
-          </Timeline.Item>
-            <Timeline.Item color="yellow" className='a-step'>
+            </Timeline.Item>
+            <Timeline.Item color='yellow' className='process-profile-step'>
               现场测试
-          </Timeline.Item>
-            <Timeline.Item color="gray" className='a-step'>
-              接受原始记录和测试报告
-          </Timeline.Item>
-            <Timeline.Item color="gray" className='a-step'>
+            </Timeline.Item>
+            <Timeline.Item color='gray' className='process-profile-step'>
+              接收原始记录和测试报告
+            </Timeline.Item>
+            <Timeline.Item color='gray' className='process-profile-step'>
               给予打分
-          </Timeline.Item>
+            </Timeline.Item>
           </Timeline>
         </div>
       </div>
-      <div className='b-box'>
-        <p className='b-top-box'>
-          <p>当前步骤-</p>
-          <p>提交上传7种材料</p>
+      <div className='process-item-box current-profile-box'>
+        <p className='title-box'>
+          <span>当前步骤</span>-<span>提交上传7种材料</span>
         </p>
-        <div className='b-bottom-box'>
-          <div className='b-inner-box'>
-            <p className='b-passage'>1.评测合同</p>
-            <Upload >
-              <Button className='b-button'>
-                <Icon type="upload" /> 上传文件
-            </Button>
-            </Upload>
-          </div>
-          <div className='b-inner-box'>
-            <p className='b-passage'>2.软件著作权证书</p>
-            <Upload >
-              <Button className='b-button'>
-                <Icon type="upload" /> 上传文件
-            </Button>
-            </Upload>
-          </div>
-          <div className='b-inner-box'>
-            <p className='b-passage'>3.样品登记表</p>
-            <Upload >
-              <Button className='b-button'>
-                <Icon type="upload" /> 上传文件
-            </Button>
-            </Upload>
-          </div>
-          <div className='b-inner-box'>
-            <p className='b-passage'>4.产品说明</p>
-            <Upload >
-              <Button className='b-button'>
-                <Icon type="upload" /> 上传文件
-            </Button>
-            </Upload>
-          </div>
-          <div className='b-inner-box'>
-            <p className='b-passage'>
-              5.用户文档集
-          </p>
-            <Upload >
-              <Button className='b-button'>
-                <Icon type="upload" />
-                上传文件
-            </Button>
-            </Upload>
-          </div>
-          <div className='b-inner-box'>
-            <p className='b-passage'>6.产品介质</p>
-            <Upload >
-              <Button className='b-button'>
-                <Icon type="upload" /> 上传文件
-            </Button>
-            </Upload>
-          </div>
-          <div className='b-inner-box'>
-            <p className='b-passage'>7.现场测试申请表</p>
-            <Upload >
-              <Button className='b-button'>
-                <Icon type="upload" /> 上传文件
-            </Button>
-            </Upload>
-          </div>
-        </div>
+        <ol className='process-profile-ol'>
+          <li>
+            <p className='current-profile-inner-left-box'>评测合同</p>
+
+            <span className='current-right-box current-upload-button'>
+              <Icon type='upload' /> 上传文件
+            </span>
+          </li>
+          <li>
+            <p className='current-profile-inner-left-box'>软件著作权证书</p>
+
+            <div className='current-right-box'>
+              <Icon
+                type='check-circle'
+                theme='twoTone'
+                twoToneColor='#52c41a'
+              />
+            </div>
+          </li>
+          <li>
+            <p className='current-profile-inner-left-box'>样品登记表</p>
+
+            <span className='current-right-box current-upload-button'>
+              <Icon type='edit' /> 填写内容
+            </span>
+          </li>
+          <li>
+            <p className='current-profile-inner-left-box'>产品说明</p>
+            <div className='current-right-box'>
+              <Icon
+                type='check-circle'
+                theme='twoTone'
+                twoToneColor='#52c41a'
+              />
+            </div>
+          </li>
+          <li>
+            <p className='current-profile-inner-left-box'>用户文档集</p>
+            <div className='current-right-box'>
+              <Icon
+                type='check-circle'
+                theme='twoTone'
+                twoToneColor='#52c41a'
+              />
+            </div>
+          </li>
+          <li>
+            <p className='current-profile-inner-left-box'>产品介质</p>
+            <div className='current-right-box'>
+              <Icon
+                type='check-circle'
+                theme='twoTone'
+                twoToneColor='#52c41a'
+              />
+            </div>
+          </li>
+          <li>
+            <p className='current-profile-inner-left-box'>现场测试申请表</p>
+            <div className='current-right-box'>
+              <Icon
+                type='check-circle'
+                theme='twoTone'
+                twoToneColor='#52c41a'
+              />
+            </div>
+          </li>
+        </ol>
       </div>
-      <div className='c-box'>
-        <p className='c-top-box'>
-          用户信息
-        </p>
-        <div className='c-bottom-box'>
-          <Icon type='user' className='c-icon' />
-          <div className='c-passage'>
+      <div className='process-item-box'>
+        <p className='title-box'>咨询者信息</p>
+        <ul className='manager-info-bottom-box'>
+          <li className='manager-info-head-box'></li>
+          <li className='manager-into-item-box'>
             <p>
-              <Icon type='user' className='c-icon2' />用户名
-          </p>
+              <Icon type='user' className='manager-info-icon' />
+              用户名
+            </p>
             <p>张博荣</p>
-          </div>
-          <div className='c-passage'>
+          </li>
+          <li className='manager-into-item-box'>
             <p>
-              <Icon type='phone' className='c-icon2' />电话
-          </p>
+              <Icon type='phone' className='manager-info-icon' />
+              电话
+            </p>
             <p>18351923820</p>
-          </div>
-          <div className='c-passage'>
+          </li>
+          <li className='manager-into-item-box'>
             <p>
-              <Icon type='tag' className='c-icon2' />办理编号
+              <Icon type='tag' className='manager-info-icon' />
+              办理业务次数
             </p>
             <p>111</p>
-          </div>
-          <div className='c-passage'>
+          </li>
+          <li className='manager-into-item-box'>
             <p>
-              <Icon type='star' className='c-icon2' />星级
-          </p>
-            <p>★★★★★</p>
-          </div>
-        </div>
+              <Icon type='star' className='manager-info-icon' />
+              星级
+            </p>
+            <Rate disabled defaultValue={4} />
+          </li>
+        </ul>
       </div>
     </div>
   );
