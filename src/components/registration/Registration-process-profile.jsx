807759bbd9deb5849b58877a19320e3c3a42a44c
Qccccc's laptop <896853205@qq.com>
2020-01-12 21:40:04 +0800

    修改bug 过程图加上loading

--- src/components/registration/Registration-process-profile.jsx
+++ src/components/registration/Registration-process-profile.jsx
@@ -5,38 +5,39 @@ import { QUERY_SYS_REGISTRATION_STEP } from '@/constants/api-constants';
 import proxyFetch from '@/util/request';
 
 // 样式
-import { Timeline } from 'antd';
+import { Timeline, Skeleton } from 'antd';
 
 export default props => {
-  const [sysRegistrationStepList, setSysRegistrationStepList] = useState(null);
+  const [sysRegistrationStepList, setSysRegistrationStepList] = useState([]),
+    [loading, setLoading] = useState(true);
 
   useEffect(() => {
     (async () => {
+      setLoading(true);
       const sysRegistrationStepList = await proxyFetch(
         QUERY_SYS_REGISTRATION_STEP,
         {},
         'GET'
       );
 
-    
-      console.log(sysRegistrationStepList);
-
       setSysRegistrationStepList(sysRegistrationStepList);
+      setLoading(false);
     })();
   }, []);
-
-
-  let list = sysRegistrationStepList
-    ? sysRegistrationStepList.map((item, i) => (
-        <Timeline.item key={i}>{item.name}</Timeline.item>
-      ))
-    : null;
 
   return (
     <div className='process-item-box'>
       <p className='title-box'>测试进度</p>
       <div className='process-profile-bottom-box'>
-        <Timeline>{list}</Timeline>}
+        <Skeleton loading={loading}>
+          <Timeline>
+            {sysRegistrationStepList
+              ? sysRegistrationStepList.map((item, i) => (
+                  <Timeline.Item key={i}>{item.name}</Timeline.Item>
+                ))
+              : null}
+          </Timeline>
+        </Skeleton>
       </div>
     </div>
   );
