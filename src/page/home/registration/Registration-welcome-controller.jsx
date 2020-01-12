d6ae7042d0ed381ecabd797c9cd62fc4f56222c9
Qccccc's laptop <896853205@qq.com>
2020-01-13 00:37:03 +0800

    登记测试页微调样式

--- src/page/home/registration/Registration-welcome-controller.jsx
+++ src/page/home/registration/Registration-welcome-controller.jsx
@@ -33,17 +33,27 @@ export default props => {
             <Icon type='audit' />
             <span>登记办理新测试</span>
           </h1>
-          <p className='passage'>
-          服务流程：
-          1. 委托方填写产品测试登记表，准备必要资料（包括产品测试功能表和产品用户操作手册）。
-          2. 委托方准备测试环境。
-          3. 开展检测工作。
-          4. 对检测过程中发现的问题进行及时沟通。
-          5. 出具检测报告。
-          6. 后续相关事宜的咨询。
-          注意事项：
-          1. 拥有软件著作权的软件，检测时运行名称须与软件著作权证书上体现的名称一致。
-          2. 尽量提前或预留检测时间，避免招投标或项目验收对检测报告的需求特别急迫。
+          <p>
+            服务流程：
+            <ol>
+              <li>
+                委托方填写产品测试登记表，准备必要资料（包括产品测试功能表和产品用户操作手册）。
+              </li>
+              <li>委托方准备测试环境。</li>
+              <li>开展检测工作。</li>
+              <li>对检测过程中发现的问题进行及时沟通。</li>
+              <li>出具检测报告。</li>
+              <li>后续相关事宜的咨询。</li>
+            </ol>
+            注意事项：
+            <ol>
+              <li>
+                拥有软件著作权的软件，检测时运行名称须与软件著作权证书上体现的名称一致。
+              </li>
+              <li>
+                尽量提前或预留检测时间，避免招投标或项目验收对检测报告的需求特别急迫。
+              </li>
+            </ol>
           </p>
         </div>
         <div className='bottom-box'>
@@ -69,7 +79,7 @@ export default props => {
       <div className='right-box'>
         <img
           className='image'
-          src='/image/welcome-controller/right.jpg'
+          src='/image/registration/welcome/right.jpg'
           alt='Paris'
         />
       </div>
