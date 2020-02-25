35dd1dcfd0ce082f94da07fc1bf4c94fa57c1829
Zbr1920410104 <765451419@qq.com>
2020-02-25 10:54:03 +0800

    添加企业端接受报告和原始记录页面和逻辑

--- src/components/registration/current/Field-tests-profile.jsx
+++ src/components/registration/current/Field-tests-profile.jsx
@@ -4,17 +4,14 @@ import React, { useState, useEffect } from 'react';
 import { useSelector } from 'react-redux';
 
 // 样式
-import { Alert, Tag, Skeleton, Button } from 'antd';
+import { Alert, Tag, Skeleton } from 'antd';
 import '@/style/home/registration/field-test.styl';
 
 // 请求
 import proxyFetch from '@/util/request';
 import {
   SELECT_TEST_REGISTRATION_APPLY,
-  SELECT_TEST_REGISTRATION_SPECIMEN,
-  GET_FILE_URL,
-  SELECT_ENTERPRISE_REGISTRATION_RECORD,
-  SELECT_ENTERPRISE_REGISTRATION_REPORT
+  SELECT_TEST_REGISTRATION_SPECIMEN
 } from '@/constants/api-constants';
 
 // 路由
@@ -27,12 +24,6 @@ export default props => {
     ),
     [getDataLoading, setGetDataLoading] = useState(false),
     [applyManagerStatus, setApplyManagerStatus] = useState(0),
-    [recordPreviewUrl, setRecordPreviewUrl] = useState(''),
-    [recordUrl, setRecordUrl] = useState(''),
-    [downloadRecordLoading, setDownloadRecordLoading] = useState(false),
-    [reportUrl, setReportUrl] = useState(''),
-    [reportPreviewUrl, setReportPreviewUrl] = useState(''),
-    [downloadReportLoading, setDownloadReportLoading] = useState(false),
     [specimenManagerStatus, setSpecimenManagerStatus] = useState(0);
 
   const getTagColor = status => {
@@ -112,74 +103,6 @@ export default props => {
     }
   }, [enterpriseRegistrationUuid]);
 
-  // 回显原始记录word
-  useEffect(() => {
-    if (enterpriseRegistrationUuid) {
-      (async () => {
-        setDownloadRecordLoading(true);
-
-        let record = await proxyFetch(
-          SELECT_ENTERPRISE_REGISTRATION_RECORD,
-          { registrationUuid: enterpriseRegistrationUuid },
-          'GET'
-        );
-
-        setRecordUrl(record?.finalUrl);
-        setDownloadRecordLoading(false);
-      })();
-    }
-  }, [enterpriseRegistrationUuid]);
-
-  useEffect(() => {
-    if (recordUrl) {
-      (async () => {
-        setDownloadRecordLoading(true);
-        const recordPreviewUrl = await proxyFetch(
-          GET_FILE_URL,
-          { fileUrl: recordUrl },
-          'GET'
-        );
-
-        setRecordPreviewUrl(recordPreviewUrl);
-        setDownloadRecordLoading(false);
-      })();
-    }
-  }, [recordUrl]);
-
-  // 回显原始记录word
-  useEffect(() => {
-    if (enterpriseRegistrationUuid) {
-      (async () => {
-        setDownloadReportLoading(true);
-
-        let report = await proxyFetch(
-          SELECT_ENTERPRISE_REGISTRATION_REPORT,
-          { registrationUuid: enterpriseRegistrationUuid },
-          'GET'
-        );
-
-        setReportUrl(report?.finalUrl);
-        setDownloadReportLoading(false);
-      })();
-    }
-  }, [enterpriseRegistrationUuid]);
-
-  useEffect(() => {
-    if (reportUrl) {
-      (async () => {
-        setDownloadReportLoading(true);
-        const reportPreviewUrl = await proxyFetch(
-          GET_FILE_URL,
-          { fileUrl: reportUrl },
-          'GET'
-        );
-
-        setReportPreviewUrl(reportPreviewUrl);
-        setDownloadReportLoading(false);
-      })();
-    }
-  }, [reportUrl]);
-
   return (
     <div className='item-box process-item-box current-profile-box'>
       <p className='title-box'>
@@ -228,53 +151,6 @@ export default props => {
                 >
                   {getTagText(applyManagerStatus)}
                 </Tag>
-              </div>
-            </div>
-            <div className='file-modify-title-box'>
-              <span>下载原始记录和现场报告</span>
-            </div>
-            <div className='download-box'>
-              <div className='record-download-left-box'>
-                {recordUrl ? (
-                  <a
-                    href={recordPreviewUrl}
-                    target='_blank'
-                    rel='noopener noreferrer'
-                  >
-                    <Button
-                      type='primary'
-                      icon='download'
-                      size='large'
-                      className='button'
-                      loading={downloadRecordLoading}
-                    >
-                      下载原始记录pdf文件
-                    </Button>
-                  </a>
-                ) : (
-                  <Button disabled>未生成原始记录</Button>
-                )}
-              </div>
-              <div className='report-download-right-box'>
-                {reportUrl ? (
-                  <a
-                    href={reportPreviewUrl}
-                    target='_blank'
-                    rel='noopener noreferrer'
-                  >
-                    <Button
-                      type='primary'
-                      icon='download'
-                      size='large'
-                      className='button'
-                      loading={downloadReportLoading}
-                    >
-                      下载现场报告pdf文件
-                    </Button>
-                  </a>
-                ) : (
-                  <Button disabled>未生成现场报告</Button>
-                )}
               </div>
             </div>
           </Skeleton>
---
+++ src/components/registration/current/Recept-report-profile.jsx
@@ -0,0 +1,155 @@
+import React, { useState, useEffect } from 'react';
+
+// redux
+import { useSelector } from 'react-redux';
+
+// 样式
+import { Button } from 'antd';
+import '@/style/home/registration/recept-report.styl';
+
+// 请求
+import proxyFetch from '@/util/request';
+import {
+  GET_FILE_URL,
+  SELECT_ENTERPRISE_REGISTRATION_RECORD,
+  SELECT_ENTERPRISE_REGISTRATION_REPORT
+} from '@/constants/api-constants';
+
+export default props => {
+  const { enterpriseRegistrationUuid } = useSelector(
+      state => state.enterpriseStore
+    ),
+    [recordPreviewUrl, setRecordPreviewUrl] = useState(''),
+    [recordUrl, setRecordUrl] = useState(''),
+    [downloadRecordLoading, setDownloadRecordLoading] = useState(false),
+    [reportUrl, setReportUrl] = useState(''),
+    [reportPreviewUrl, setReportPreviewUrl] = useState(''),
+    [downloadReportLoading, setDownloadReportLoading] = useState(false);
+
+  // 回显原始记录word
+  useEffect(() => {
+    if (enterpriseRegistrationUuid) {
+      (async () => {
+        setDownloadRecordLoading(true);
+
+        let record = await proxyFetch(
+          SELECT_ENTERPRISE_REGISTRATION_RECORD,
+          { registrationUuid: enterpriseRegistrationUuid },
+          'GET'
+        );
+
+        setRecordUrl(record?.finalUrl);
+        setDownloadRecordLoading(false);
+      })();
+    }
+  }, [enterpriseRegistrationUuid]);
+
+  useEffect(() => {
+    if (recordUrl) {
+      (async () => {
+        setDownloadRecordLoading(true);
+        const recordPreviewUrl = await proxyFetch(
+          GET_FILE_URL,
+          { fileUrl: recordUrl },
+          'GET'
+        );
+
+        setRecordPreviewUrl(recordPreviewUrl);
+        setDownloadRecordLoading(false);
+      })();
+    }
+  }, [recordUrl]);
+
+  // 回显原始记录word
+  useEffect(() => {
+    if (enterpriseRegistrationUuid) {
+      (async () => {
+        setDownloadReportLoading(true);
+
+        let report = await proxyFetch(
+          SELECT_ENTERPRISE_REGISTRATION_REPORT,
+          { registrationUuid: enterpriseRegistrationUuid },
+          'GET'
+        );
+
+        setReportUrl(report?.finalUrl);
+        setDownloadReportLoading(false);
+      })();
+    }
+  }, [enterpriseRegistrationUuid]);
+
+  useEffect(() => {
+    if (reportUrl) {
+      (async () => {
+        setDownloadReportLoading(true);
+        const reportPreviewUrl = await proxyFetch(
+          GET_FILE_URL,
+          { fileUrl: reportUrl },
+          'GET'
+        );
+
+        setReportPreviewUrl(reportPreviewUrl);
+        setDownloadReportLoading(false);
+      })();
+    }
+  }, [reportUrl]);
+
+  return (
+    <div className='item-box process-item-box current-profile-box'>
+      <p className='title-box'>
+        <span>当前步骤</span>-<span>现场测试</span>
+      </p>
+      <div className='recept-report-box'>
+        <div className='recept-report-detail-box'>
+          <div className='recept-report-title-box'>
+            <span>接受原始记录和现场报告</span>
+          </div>
+          <div className='download-box'>
+            <div className='record-download-left-box'>
+              {recordUrl ? (
+                <a
+                  href={recordPreviewUrl}
+                  target='_blank'
+                  rel='noopener noreferrer'
+                >
+                  <Button
+                    type='primary'
+                    icon='download'
+                    size='large'
+                    className='button'
+                    loading={downloadRecordLoading}
+                  >
+                    下载原始记录pdf文件
+                  </Button>
+                </a>
+              ) : (
+                <Button disabled>未生成原始记录</Button>
+              )}
+            </div>
+            <div className='report-download-right-box'>
+              {reportUrl ? (
+                <a
+                  href={reportPreviewUrl}
+                  target='_blank'
+                  rel='noopener noreferrer'
+                >
+                  <Button
+                    type='primary'
+                    icon='download'
+                    size='large'
+                    className='button'
+                    loading={downloadReportLoading}
+                  >
+                    下载现场报告pdf文件
+                  </Button>
+                </a>
+              ) : (
+                <Button disabled>未生成现场报告</Button>
+              )}
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
